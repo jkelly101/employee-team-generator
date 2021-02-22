57fcf1e5a0b9f9dc4ff60b634d99a8c343b81f2d
jkelly101 <jenLkelly@gmail.com>
2021-02-22 13:56:38 -0500

    added class Employee wwith constructors and module exports to Employee.js.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(id, name, email){
+        this.id = id,
+    this.name = name,
+    this.email = email
+    }
+}
+
+module.exports = Employee;
+
+// The first class is an `Employee` parent class with the following properties and
+// methods:
+
+//   * name
+//   * id
+//   * email
+//   * getName()
+//   * getId()
+//   * getEmail()
+//   * getRole() // Returns 'Employee'
